80d6d7fb9825c944952f22f3be7b88af1e18688a
余晔 <18838280615@163.com>
2025-08-08 00:11:56 +0800

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,31 @@
-'use client'
-
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
+export default function Page() {
   return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
+    <div className="flex flex-col gap-2 items-center justify-center h-screen bg-gray-300">
+      <h1 className="text-3xl bold">Welcome to web3 game BlackJack</h1>
+      <h2 className="text-2xl bold">Message: Player wins/ dealer Wins: BlackJack / bust!</h2>
+      <div className="mt-4">
+        <h2>Dealer`s hand</h2>
+        <div className="flex flex-row gap-2">
+          <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 1</div>
+          <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 2</div>
+          <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 3</div>
+        </div>
         </div>
 
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
+        <div>
+          <h2>Player`s hand</h2>
+          <div className="flex flex-row gap-2">
+            <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 1</div>
+            <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 2</div>
+            <div className="w-32 h-42 border-1 border-black bg-white rounded-md">Card 3</div>
+          </div>
+        </div>
 
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+        <div className="flex flex-row gap-2 mt-4">
+          <button className="bg-amber-300 p-2 rounded-md">Hit</button>
+          <button className="bg-amber-300 p-2 rounded-md">Stand</button>
+          <button className="bg-amber-300 p-2 rounded-md">Reset</button>
+        </div>
+    </div>
+  );
 }
-
-export default App
